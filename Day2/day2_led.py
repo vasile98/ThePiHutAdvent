70936e6db15e4bd813d99dc4230b2971c7605bbf
Vasi <vaseoc98@gmail.com>
2022-12-07 22:42:24 +0200

    Added Pin layout for Pico H and finished activity 1&2 for Day2

---
+++ Day2/day2_led.py
@@ -0,0 +1,29 @@
+from machine import Pin
+import time
+
+red = Pin(18,Pin.OUT)
+green = Pin(19,Pin.OUT)
+amber = Pin(20,Pin.OUT)
+
+#Number of iterations for the  while loop 
+counter = 0
+
+while counter < 10 :
+    print(counter)
+    
+    # Turn the leds on 
+    red.value(1)
+    green.value(1)
+    amber.value(1)
+    
+    # Wait for half a second 
+    time.sleep(0.5)
+    
+    #Leds on 
+    red.value(0)
+    green.value(0)
+    amber.value(0)
+    
+    time.sleep(0.5)
+    counter +=1
+    
